203e87d9e50ea80b5586f63fbb812cf37e37947f
Alan Bettis <alanbettis56@gmail.com>
2020-02-20 08:03:23 -0500

    Update HandposeRecord.py
    Hold 'q' to record hand poses, hit spacebar to change between recording open and closed hands, and hit 'p' to run t-SNE on the data gathered in that run.

--- recorder/HandposeRecord.py
+++ recorder/HandposeRecord.py
@@ -1,6 +1,6 @@
 # A Hello World
 # 
-# This code acquired directly from https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-python.html
+# This code modified from https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-python.html
 # 
 # Written by the Leapmotion devs as a "Hello World" for their system.
 
@@ -14,28 +14,58 @@ arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
 import Leap, keyboard, csv
+import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+def normalize(coordinates, normal, colinear):
+    #Arrange array for ease of modification
+    coordinates = np.reshape(np.asarray(coordinates), (-1, 3))
+    normal = np.asarray(normal)
+    colinear = np.asarray(colinear)
+
+    #normalize translation
+    coordinates = coordinates - coordinates[0]
+
+    #calculate cross product between normal and colinear
+    cross = np.cross(normal, colinear)
+    normal = normal.reshape((-1, 1))
+    colinear = colinear.reshape((-1, 1))
+    cross = cross.reshape((-1, 1))
+
+    #normalize rotation
+    for i in range(len(coordinates)):
+        #coordinates[i] = coordinates[i][0] * normal + coordinates[i][1] * colinear + coordinates[i][2] * cross
+        coordinates[i] = np.linalg.inv(np.concatenate((normal, colinear, cross), axis=1)).dot(coordinates[i])
+
+    #convert back to list and return
+    coordinates = coordinates.flatten().tolist()
+    return coordinates
 
 def get_vector(vec):
-    print "x: " + str(vec.x)
-    print "y: " + str(vec.y)
-    print "z: " + str(vec.z)
+    print ("x: " + str(vec.x))
+    print ("y: " + str(vec.y))
+    print ("z: " + str(vec.z))
     
     return([vec.x, vec.y, vec.z])
 
 def get_finger_joints(finger):
-    print "----Knuckle position"
+    print ("----Knuckle position")
     knuckle = get_vector(finger.bone(Leap.Bone.TYPE_METACARPAL).prev_joint)
 
-    print "----Distal Metacarpal position"
+    print ("----Distal Metacarpal position")
     metacarpal = get_vector(finger.bone(Leap.Bone.TYPE_METACARPAL).next_joint)
 
-    print "----Distal Proximal Phalanx position"
+    print ("----Distal Proximal Phalanx position")
     proximal = get_vector(finger.bone(Leap.Bone.TYPE_PROXIMAL).next_joint)
 
-    print "----Distal Intermediate Phalanx position"
+    print ("----Distal Intermediate Phalanx position")
     intermediate = get_vector(finger.bone(Leap.Bone.TYPE_INTERMEDIATE).next_joint)
 
-    print "----Distal Distal Phalanx position"
+    print ("----Distal Distal Phalanx position")
     distal = get_vector(finger.bone(Leap.Bone.TYPE_DISTAL).next_joint)
     
     finger_joints = []
@@ -63,6 +93,7 @@ def get_finger_joints(finger):
     return finger_joints
 
 def get_hand_position_data(hand):
+    #Get fingers
     thumb_finger_list = hand.fingers.finger_type(Leap.Finger.TYPE_THUMB)
     thumb = thumb_finger_list[0]
 
@@ -78,64 +109,106 @@ def get_hand_position_data(hand):
     pinky_finger_list = hand.fingers.finger_type(Leap.Finger.TYPE_PINKY)
     pinky = pinky_finger_list[0]
 
-    #Print hand position
+    #Print hand position and get whether hand is left or right
     is_left = 0
     if hand.is_left:
         is_left = 1
-        print "Left Hand palm position:"
+        print ("Left Hand palm position:")
     else:
         is_left = 0
-        print "Right Hand palm position:"
+        print ("Right Hand palm position:")
+    #Get palm position and normal vector and colinear vector
     palm_pos = get_vector(hand.palm_position)
+    normal_vector = get_vector(hand.palm_normal)
+    colinear_vector = get_vector(hand.direction)
 
-    print "\nThumb joint positions:"
+    #Get joints
+    print ("\nThumb joint positions:")
     thumb_pos = get_finger_joints(thumb)
-    print "\nIndex joint positions:"
+    print ("\nIndex joint positions:")
     index_pos = get_finger_joints(index)
-    print "\nMiddle joint positions:"
+    print ("\nMiddle joint positions:")
     middle_pos = get_finger_joints(middle)
-    print "\nRing joint positions:"
+    print ("\nRing joint positions:")
     ring_pos = get_finger_joints(ring)
-    print "\nPinky joint positions:"
+    print ("\nPinky joint positions:")
     pinky_pos = get_finger_joints(pinky)
-    
+
+    #Concat arrays to get a list of joint positions
     row = palm_pos + thumb_pos + index_pos + middle_pos + ring_pos + pinky_pos
-    row.insert(0, is_left)
-    
+    #Pass joints, normal vector of palm, and colinear hand vector to normalize function to achieve rotation/translation invariance
+    row = normalize(row, normal_vector, colinear_vector)
+    #Insert whether the hand is left at the start of the array
+    #row.insert(0, is_left)
+
+    #Add row to .csv file
     print(row)
-    with open('out.csv', 'ab') as file:
+    with open('raw_out.csv', 'ab') as file:
         writer = csv.writer(file)
         writer.writerow(row)
 
+    return row
+
 unlocked = True
+data = [[], []]
+dataIndex = 0
     
 class SampleListener(Leap.Listener):
-
     def on_connect(self, controller):
-        print "Connected"
+        print ("Connected")
 
     def on_frame(self, controller):
         global unlocked
-        #try:
-        if keyboard.is_pressed('q') and unlocked:
+        global data
+        global dataIndex
+        
+        if keyboard.is_pressed('space') and unlocked:
             unlocked = False
-            print "Key pressed!"
+            if dataIndex == 0:
+                dataIndex = 1
+                print("Now recording closed hands")
+            else:
+                dataIndex = 0
+                print("Now recording open hands")
+        if keyboard.is_pressed('q') and unlocked:
+            print ("Key pressed!")
             frame = controller.frame()
             if frame.is_valid:
-                print "frame collected!"
                 hands = frame.hands
                 handL = None
                 handR = None
                 for i in range(0,2):
                     if hands[i].is_valid:
-                        print "HAND FOUND"
+                        print ("HAND FOUND")
                         if hands[i].is_left:
                             handL = hands[i]
                             row = get_hand_position_data(handL)
+                            if None not in row:
+                                data[dataIndex].append(row)
                         elif hands[i].is_right:
                             handR = hands[i]
                             row = get_hand_position_data(handR)
-        elif not keyboard.is_pressed('q') and not unlocked:
+                            if None not in row:
+                                data[dataIndex].append(row)
+        if keyboard.is_pressed('p') and unlocked:
+            unlocked = False
+            print("Starting Learning...")
+            for i in range(len(data)):
+                data[i] = np.asarray(data[i])
+                data[i] = TSNE().fit_transform(data[i])
+                for j in range(len(data[i])):
+                    with open('TSNE_out.csv', 'ab') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(data[i][j].tolist())
+            print(data)
+            plt.plot(data[0][:,0], data[0][:,1], 'ro', data[1][:,0], data[1][:,1], 'bx')
+            red_patch = mpatches.Patch(color='red', label='Open hand positions')
+            blue_patch = mpatches.Patch(color='blue', label='Closed hand positions')
+            plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+            plt.legend(handles=[red_patch, blue_patch])
+            plt.show()
+
+        if not keyboard.is_pressed('space') and not keyboard.is_pressed('p') and not unlocked:
             unlocked = True
         
 
@@ -149,7 +222,7 @@ def main():
         unlocked = True
         
         # Keep this process running until Enter is pressed
-        print "Press enter to quit or 'q' to record frame"
+        print ("Press enter to quit or 'q' to record frame")
         try:
             sys.stdin.readline()
         except KeyboardInterrupt:
